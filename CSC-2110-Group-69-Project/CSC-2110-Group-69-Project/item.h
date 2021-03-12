661f07afc7711e7b53bc3bd1a36aeab5d931f7d4
nrastogi1 <80496148+nrastogi1@users.noreply.github.com>
2021-03-11 21:28:50 -0500

    Update item.h

--- CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/item.h
+++ CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/item.h
@@ -1,1 +1,18 @@
+// Neil
 
+#include <cstdlib> // For several general-purpose functions
+#include <fstream> // For file handling
+#include <iomanip> // For formatted output
+#include <iostream> // For cin, cout, and system
+#include <string> // For string data type
+#include <ctype.h> // For alphanumeric check
+using namespace std; // So "std::cout" may be abbreviated to "cout"
+
+
+class Item
+{
+private:
+int type;
+int value;
+public:
+bool isHP()
