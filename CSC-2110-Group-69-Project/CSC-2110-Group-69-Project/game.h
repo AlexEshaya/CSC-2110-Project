4a024e07c7eea549caabfcd0ea107b4c127759b4
LucasJAHH <80496226+LucasJAHH@users.noreply.github.com>
2021-03-12 15:21:13 -0500

    Update game.h

--- CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/game.h
+++ CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/game.h
@@ -2,5 +2,14 @@
 
 class game{
   private:
+	//private variables
+		Room* currentRoom;
+		Hero HeroName;
+	//private functions
+		void createMap();
   public:
+	//public functions
+		void run();
+	//constructors
+		Game();
 };
