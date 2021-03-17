c6c0a311d94cbaaa6d26f3be04fefe869533b027
perzuazn <80496198+perzuazn@users.noreply.github.com>
2021-03-16 23:36:42 -0400

    Update hero.cpp

--- CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/hero.cpp
+++ CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/hero.cpp
@@ -1,4 +1,8 @@
-#include <string>
-#include <iostream>
+#include <cstdlib> // For several general-purpose functions
+#include <fstream> // For file handling
+#include <iomanip> // For formatted output
+#include <iostream> // For cin, cout, and system
+#include <string> // For string data type
+using namespace std; // So "std::cout" may be abbreviated to "cout"
 
 #include "hero.h"
