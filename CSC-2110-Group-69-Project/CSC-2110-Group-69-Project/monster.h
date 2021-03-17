023b4efde26ad53588b2504c7501614a937904fb
perzuazn <80496198+perzuazn@users.noreply.github.com>
2021-03-16 23:02:16 -0400

    Update monster.h

--- CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/monster.h
+++ CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/monster.h
@@ -1,1 +1,13 @@
+#pragma once
+#include <string>
+#include <iostream>
 
+#include "character.h"
+
+class monster:
+{
+  public:
+  void decreaseAttackPower(); // The decreaseAttackPower function decreases attackPower by 1
+  
+  Monster(int,int); // The arguments of the Monster’s constructor are hp, and attack power
+};
