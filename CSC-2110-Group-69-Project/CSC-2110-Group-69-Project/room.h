192122590248d17d22f67ff136c7024e9f5ae352
perzuazn <80496198+perzuazn@users.noreply.github.com>
2021-03-16 22:36:46 -0400

    Update room.h

--- CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/room.h
+++ CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/room.h
@@ -1,1 +1,41 @@
+#pragma once
+#include <string>
+#include <iostream>
 
+class Room:
+{
+  private:
+  string name;
+  
+  Room* east;
+  Room* west;
+  Room* north;
+  Room* south;
+  
+  Monster* roomMonster;
+  Item* roomItem;
+  bool exit;
+  
+  public:
+  void setName(string);
+  string getName() const;
+  void setEastRoom(Room*);
+  void setWestRoom(Room*);
+  void setNorthRoom(Room*);
+  void setSouthRoom(Room*);
+  Room* getEastRoom() const;
+  Room* getWestRoom() const;
+  Room* getNorthRoom() const;
+  Room* getSouthRoom() const;
+  void setRoomMonster(Monster*);
+  Monster* getRoomMonster() const;
+  void setRoomItem(Item*);
+  Item* getRoomItem() const;
+  bool isExit();
+  
+  // Construct with parameters!
+  Room(string, Room*,Room*,Room*,Room*,Monster*,Item*);
+  Room(string, Room*,Room*,Room*,Room*,Monster*);
+  Room(string, Room*,Room*,Room*,Room*,Item*);
+  Room(string, Room*,Room*,Room*,Room*,bool);
+};
