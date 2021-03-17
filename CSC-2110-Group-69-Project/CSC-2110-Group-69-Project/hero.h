e1dc76ced474fde8deb4fc082fbc348b5ce1668e
perzuazn <80496198+perzuazn@users.noreply.github.com>
2021-03-16 22:57:15 -0400

    Update hero.h

--- CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/hero.h
+++ CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/hero.h
@@ -1,6 +1,25 @@
 #pragma once
+#include <string>
+#include <iostream>
 
-class hero{
+#include "character.h"
+
+class Hero:
+{
   private:
+  int defencePower;
+  int retreatCount;
+  
   public:
+  Item*[3] bag;
+  int getDefensePower() const;
+  void decreaseDefensePower();
+  void setDefensePower(int);
+  int getRetreatCount() const;
+  void decreaseRetreatCount();
+  bool anyItems();
+  bool isAlive() const;
+  int operator*(Monster& m);
+  
+  Hero(int,int,int,int);
 };
