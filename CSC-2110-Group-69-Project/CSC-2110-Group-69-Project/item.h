828746625399e6fa8cfbc9989b52fc9a5f3e07fd
nrastogi1 <80496148+nrastogi1@users.noreply.github.com>
2021-03-22 23:59:54 -0400

    Update item.h

--- CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/item.h
+++ CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/item.h
@@ -1,4 +1,3 @@
-// Neil
 #pragma once
 #include <cstdlib> // For several general-purpose functions
 #include <fstream> // For file handling
@@ -8,15 +7,16 @@
 #include <ctype.h> // For alphanumeric check
 using namespace std; // So "std::cout" may be abbreviated to "cout"
 
-
+// Declare Item Class
 class Item
 {
 private:
   int type; // 0 = hp, 1 = defense, 2 = attack. An ATTACK ITEM will be our unique feature for the game.
-  int value;
+  int value; // Will dictate the value of the hp, defense, or attack item.
 public:
-  bool isHP() const;
-  bool isDefense() const;
-  int getValue() const;
+  bool isHP() const; // Checks to see if item is an HP item
+  bool isDefense() const; // Checks to see if item is a Defense item
+  bool isAttack() const; // Checks to see if item is an attack item
+  int getValue() const; // Outputs value of item. This is an accessor, the item class does not have a mutator for value.
   Item(int, int); // arguments are type and value
 }
