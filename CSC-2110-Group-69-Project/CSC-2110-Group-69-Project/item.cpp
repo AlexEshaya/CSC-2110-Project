4a1a07c60e54444d7f7e6e353d1de31b7f339974
LucasJAHH <80496226+LucasJAHH@users.noreply.github.com>
2021-03-23 00:02:23 -0400

    Update item.cpp

--- CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/item.cpp
+++ CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/item.cpp
@@ -1,10 +1,3 @@
 // Neil
-
-#include <cstdlib> // For several general-purpose functions
-#include <fstream> // For file handling
-#include <iomanip> // For formatted output
-#include <iostream> // For cin, cout, and system
-#include <string> // For string data type
-#include <ctype.h> // For alphanumeric check
-using namespace std; // So "std::cout" may be abbreviated to "cout"
+#include "item.h"
 
