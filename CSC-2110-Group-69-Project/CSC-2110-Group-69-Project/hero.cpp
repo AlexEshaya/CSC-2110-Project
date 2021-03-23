1c5b56b98d39ec6dbd9973fc5a522988cbf1811a
LucasJAHH <80496226+LucasJAHH@users.noreply.github.com>
2021-03-22 23:58:47 -0400

    Update hero.cpp

--- CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/hero.cpp
+++ CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/hero.cpp
@@ -1,1 +1,35 @@
 #include "hero.h"
+
+//constructor
+Hero::Hero(int a, int b , int c, int d) {
+
+}
+
+//functions
+const int Hero::getDefensePower() {
+
+}
+
+void Hero::decreaseDefensePower() {
+
+}
+
+void Hero::setDefensePower(int) {
+
+}
+
+const int Hero::getRetreatCount() {
+
+}
+
+void Hero::decreaseRetreatCount() {
+
+}
+
+const bool Hero::anyItems() {
+
+}
+
+const bool Hero::isAlive() {
+
+}
