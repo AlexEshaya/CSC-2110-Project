f07b6bd46a951d0c316859b64c9aab7a4d359562
AlexEshaya <80801196+AlexEshaya@users.noreply.github.com>
2021-03-24 23:32:10 -0400

    Update game.h

--- CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/game.h
+++ CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/game.h
@@ -1,17 +1,22 @@
-#pragma once
+#include "room.h"
+#include "hero.h"
+#include <string>
+#include <iostream>
+using namespace std;
 
-class game{
-  private:
+class game
+{
+ private:
 	//private variables
-		Room* previousRoom;
-		Room* currentRoom;
-		Hero HeroName;
-		char input;
+	Room* previousRoom;
+	Room* currentRoom;
+	//Hero HeroName = Hero(100, 100, 100, 100);
+	char input;
 	//private functions
-		void createMap();
-  public:
+	void createMap();
+ public:
 	//public functions
-		void run();
+	void run();
 	//constructors
-		Game();
+	game();
 };
