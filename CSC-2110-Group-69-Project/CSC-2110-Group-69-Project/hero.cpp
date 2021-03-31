d3cb850880cd5e1367bd5badfd886d1afa1f342f
perzuazn <80496198+perzuazn@users.noreply.github.com>
2021-03-30 20:22:34 -0400

    Update hero.cpp

--- CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/hero.cpp
+++ CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/hero.cpp
@@ -1,35 +1,59 @@
 #include "hero.h"
 
 //constructor
-Hero::Hero(int a, int b , int c, int d) {
-
+Hero::Hero(int hp, int attackPower, int defensePower, int retreatCount)
+{
+  Item*[3] bag;
 }
 
 //functions
-const int Hero::getDefensePower() {
-
+const int Hero::getDefensePower()
+{
+  
 }
 
-void Hero::decreaseDefensePower() {
-
+void Hero::decreaseDefensePower()
+{
+  defensePower = defensePower - 1;
 }
 
-void Hero::setDefensePower(int) {
-
+void Hero::setDefensePower(int)
+{
+  
 }
 
-const int Hero::getRetreatCount() {
-
+const int Hero::getRetreatCount()
+{
+  
 }
 
-void Hero::decreaseRetreatCount() {
-
+void Hero::decreaseRetreatCount()
+{
+  retreatCount = retreatCount - 1;
 }
 
-const bool Hero::anyItems() {
-
+const bool Hero::anyItems()
+{
+  if (Item*[3] bag = NULL)
+  {
+    return false;
+  }
+  
+  else
+  {
+    return true;
+  }
 }
 
-const bool Hero::isAlive() {
-
+const bool Hero::isAlive()
+{
+  if (hp > 0)
+  {
+    return true;
+  }
+  
+  else
+  {
+    return false;
+  }
 }
