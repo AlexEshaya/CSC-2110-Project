a3aa4c4133397b09ed38a4d2f22a2fdd04458967
LucasJAHH <80496226+LucasJAHH@users.noreply.github.com>
2021-03-30 22:37:53 -0400

    Update monster.cpp

--- CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/monster.cpp
+++ CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/monster.cpp
@@ -2,15 +2,33 @@
 
 Monster::Monster(int hP, int aP)
 {
-  hp = hP;
-  
-  if (aP => 1)
-  {
-    attackPower = aP;
-  }
+    health = hP;
+
+    if (aP >= 1)
+    {
+        attackPower = aP;
+    }
+    else {
+        attackPower = 0;
+    }
 }
 
 void Monster::decreaseAttackPower()
 {
-  attackPower = attackPower - 1;
+    attackPower = attackPower - 5;
+}
+void Monster::setHP(int h) {
+    health = h;
+}
+int Monster::getHP() const {
+    return health;
+}
+
+//AttackPower mutator and accessor
+void Monster::setAttackPower(int ap) {
+    attackPower = ap;
+}
+
+int Monster::getAttackPower() const {
+    return attackPower;
 }
