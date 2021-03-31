da7f4e580682deb47c6927bbe63a1d8718b4482c
LucasJAHH <80496226+LucasJAHH@users.noreply.github.com>
2021-03-31 00:06:22 -0400

    Update hero.cpp

--- CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/hero.cpp
+++ CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/hero.cpp
@@ -1,98 +1,102 @@
 #include "hero.h"
 
 // constructor with parameters!
+Hero::Hero() {
+
+}
 Hero::Hero(int hP, int aP, int dP, int rC)
 {
-  hp = hP;
-  attackPower = aP;
-  defensePower = dP;
-  retreatCount = rC;
-  Item*[3] bag;
+    health = hP;
+    attackPower = aP;
+    defensePower = dP;
+    retreatCount = rC;
+    Item* bag[3];
 }
 
 // functions!
 int operator*(Monster& m)
 {
-  Monster.hp = Monster.hp - hero.attackPower < 0 ? 0 : Monster.hp-hero.attackPower;
-  hero.hp = hero.hp - Monster.attackPower/hero.defensePower;
-  
-  Monster.decreaseAttackPower;
-  hero.decreaseDefensePower;
-  
-  if (hero.hp == 0) // hero died
-  {
-    return -1;
-  }
-  
-  else if (Monster.hp == 0)  // monster died
-  {
-    return 1;
-  }
-  
-  else   // no character died
-  {
-    return 0;
-  }
+   /*Monster.hp = Monster.hp - hero.attackPower < 0 ? 0 : Monster.hp - hero.attackPower;
+    hero.hp = hero.hp - Monster.attackPower / hero.defensePower;
+
+    Monster.decreaseAttackPower;
+    hero.decreaseDefensePower;
+
+    if (hero.hp == 0) // hero died
+    {
+        return -1;
+    }
+
+    else if (Monster.hp == 0)  // monster died
+    {
+        return 1;
+    }
+
+    else   // no character died
+    {
+        return 0;
+    }*/
 }
 
-const int Hero::getDefensePower()
+int Hero::getDefensePower() const
 {
-  return defensePower;
+    return defensePower;
 }
 
 void Hero::decreaseDefensePower()
 {
-  defensePower = defensePower - 1;
+    defensePower = defensePower - 1;
 }
 
 void Hero::setDefensePower(int dP)
 {
-  if (dP => 1)
-  {
-    defensePower = dP;
-  }
+    if (dP >= 1)
+    {
+        defensePower = dP;
+    }
 }
 
-const int Hero::getRetreatCount()
+int Hero::getRetreatCount() const
 {
-  return retreatCount;
+    return retreatCount;
 }
 
 void Hero::decreaseRetreatCount()
 {
-  retreatCount = retreatCount - 1;
+    retreatCount = retreatCount - 1;
 }
 
-const bool Hero::anyItems()
+ bool Hero::anyItems() const
 {
-  if (Item*[3] bag = NULL)
-  {
-    return false;
-  }
-  
-  else
-  {
-    return true;
-  }
+    if (Item* bag[3] = NULL)
+    {
+        return false;
+    }
+
+    else
+    {
+        return true;
+    }
 }
 
-const bool Hero::isAlive()
+bool Hero::isAlive() const
 {
-  if (hp > 0)
-  {
-    return true;
-  }
-  
-  else
-  {
-    return false;
-  }
+    if (health > 0)
+    {
+        return true;
+    }
+
+    else
+    {
+        return false;
+    }
 }
 
 void Hero::setHP(int h)
 {
     health = h;
 }
+
 int Hero::getHP() const
 {
     return health;
@@ -108,3 +112,4 @@ int Hero::getAttackPower() const
 {
     return attackPower;
 }
+
