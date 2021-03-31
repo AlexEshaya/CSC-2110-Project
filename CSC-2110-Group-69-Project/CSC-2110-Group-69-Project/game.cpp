fed2aa4bbb35c3b674f45cfffb647c02c1416e61
LucasJAHH <80496226+LucasJAHH@users.noreply.github.com>
2021-03-31 00:04:30 -0400

    Update game.cpp

--- CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/game.cpp
+++ CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/game.cpp
@@ -2,51 +2,70 @@
 
 game::game() {
 	
+	createMap();
 }
 
 void game::createMap() {
 	Item hpUP(0, 100);
 	Item dfUP(1, 50);
 	Item atkUP(2, 30);
+	*items[0] = hpUP;
+	*items[1] = dfUP;
+	*items[2] = atkUP;
+
 	Monster m1(500, 45);
 	Monster m2(500, 45);
 	Monster m3(500, 45);
 	Monster m4(500, 45);
+	*monsters[0] = m1;
+	*monsters[1] = m2;
+	*monsters[2] = m3;
+	*monsters[3] = m4;
 
 	//initialize starting room
-	Room start("Start", NULL ,NULL ,NULL ,NULL ,true);
-	*monstrPtr = m1;
+	Room start("Start", NULL ,NULL ,NULL ,NULL ,true);//may need to change to nullptr??
 	//room 2
-	Room two("Room 2", NULL, NULL, NULL, NULL, monstrPtr);
-	*itemPtr = hpUP;
+	Room two("Room 2", NULL, NULL, NULL, NULL, monsters[0]);
 	//room 3
-	Room three("Room 3", NULL, NULL, NULL, NULL, itemPtr);
+	Room three("Room 3", NULL, NULL, NULL, NULL, items[0]);
 	//4a
-	*itemPtr = dfUP;
-	*monstrPtr = m2;
-	Room fourA("Room 4", NULL, NULL, NULL, NULL, monstrPtr, itemPtr);
+	Room fourA("Room 4", NULL, NULL, NULL, NULL, monsters[1], items[1]);
 	//4b
-	*monstrPtr = m3;
-	Room fourB("Room 4", NULL, NULL, NULL, NULL, monstrPtr);
+	Room fourB("Room 4", NULL, NULL, NULL, NULL, monsters[2]);
 	//room 5
-	*itemPtr = atkUP;
-	Room five("Room 5", NULL, NULL, NULL, NULL, itemPtr);
+	Room five("Room 5", NULL, NULL, NULL, NULL, items[2]);
 	//room 6
-	*monstrPtr = m4;
-	Room six("Room 6", NULL, NULL, NULL, NULL, monstrPtr);	
+	Room six("Room 6", NULL, NULL, NULL, NULL, monsters[3]);
 	//initialize end room
 	Room end("Exit Room", NULL, NULL, NULL, NULL, false);
 
 	//map build
 	*rPtr = two;
 	start.setEastRoom(rPtr);
-	//*rPtr
+
+	*rPtr = three;
+	two.setSouthRoom(rPtr);
+
+	*rPtr = fourA;
+	three.setEastRoom(rPtr);
+
+	*rPtr = fourB;
+	three.setSouthRoom(rPtr);
+
+	*rPtr = five;
+	fourB.setSouthRoom(rPtr);
+
+	*rPtr = six;
+	five.setEastRoom(rPtr);
+
+	*rPtr = end;
+	six.setSouthRoom(rPtr);
+
+	*currentRoom = start;
 }
 
 void game::run() {
-	//create map
-	createMap();
-
+	Hero heroName(100, 20, 10, 3); // fix inheritence later!
 	previousRoom = NULL;
 	
 	//While (hero.alive and !currentRoom.isExit){
@@ -78,7 +97,7 @@ void game::run() {
 	//}
 
 	//run while player is alive and not in exit room
-	/*while (heroName.isAlive() && !(currentRoom->isExit()) == true) {
+	while (heroName.isAlive() && !(currentRoom->isExit()) == true) {
 
 
 		//current room information display//
@@ -113,8 +132,71 @@ void game::run() {
 			}
 		}
 
-		//item use prompt
+		//USE ITEM PROCESS
+		if (heroName.anyItems() == true) {
+			
+			//Prints bag contents
+			for (int i = 0; i < 3; i++) {
+				if (heroName.bag[i]->isHP() == true) {
+					cout << "Slot " << i + 1 << ": HP Item" << endl;
+				}
+				else if (heroName.bag[i]->isDefense() == true) {
+					cout << "Slot " << i + 1 << ": Defense Up Item" << endl;
+				} 
+				else if (heroName.bag[i]->isAttack() == true) {
+					cout << "Slot " << i + 1 << ": Attack Up Item" << endl;
+				} else {
+					cout << "Slot " << i + 1 << ": Empty Slot" << endl;
+				}
+			}
 
+			//Process to actually use an item
+			char useItemInput;
+			int slotNumberInput;
+			
+			cout << "Use Item (y/n)?";
+			cin >> useItemInput;
+
+			// While loop validates user input, if it is an incorrect an error message is produced. It then requests a new input.
+			while (useItemInput != 'y' || 'n')
+			{
+				cout << "You have entered an incorrect option. Choose 'y' to use an item, or 'n' to not use an item: ";
+				cin >> useItemInput;
+			}
+			
+
+			if(useItemInput == 'y')
+			{
+				// If user chose to use an item, the item slot number is requested.
+				cout << "Enter slot number (1 - 3)? ";
+				cin >> slotNumberInput;
+				
+				// While loop validates user input, if it is an incorrect an error message is produced. It then requests a new input.
+				while (slotNumberInput != 1 || 2 || 3)
+				{
+					cout << "You have entered an incorrect slot number. For slot 1, enter '1', for slot 2 enter '2', for slot 3 enter '3': ";
+					cin >> slotNumberInput;
+				}
+				cout << endl;
+				
+				switch (slotNumberInput-1) {
+				case 0:
+					if () {
+
+					}
+					break;
+				case 1:
+					break;
+				case 2:
+					break;
+				}
+
+			}
+			else if (useItemInput == 'n')
+			{
+				cout << "You have chosen not to use an item." << endl;
+			}
+		}
 		//fight
 
 		//death check
@@ -131,4 +213,4 @@ void game::run() {
 		cout << "Game over";
 	}
 
-}*/
+}
