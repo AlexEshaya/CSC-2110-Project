1d8fb64afeca93a4dafc08738c88b32db1088291
LucasJAHH <80496226+LucasJAHH@users.noreply.github.com>
2021-03-30 22:37:35 -0400

    Update monster.h

--- CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/monster.h
+++ CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/monster.h
@@ -1,9 +1,18 @@
 #pragma once
 #include "character.h"
 
-class Monster : public Character
-{
-  public:
-  void decreaseAttackPower();
-  Monster(int hp, int attackPower);
+class Monster : public Character{
+public:
+	//constructor
+	Monster(int, int);
+
+	void decreaseAttackPower();
+
+	//health mutator and accessor
+	void setHP(int);
+	int getHP() const;
+
+	//AttackPower mutator and accessor
+	void setAttackPower(int);
+	int getAttackPower() const;
 };
