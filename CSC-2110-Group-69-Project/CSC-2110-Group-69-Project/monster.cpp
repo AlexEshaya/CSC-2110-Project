607fa22c727d64864f9bfd5e45eb882d9d4a7bb5
perzuazn <80496198+perzuazn@users.noreply.github.com>
2021-04-08 19:27:56 -0400

    Update monster.cpp

--- CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/monster.cpp
+++ CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/monster.cpp
@@ -1,5 +1,6 @@
 #include "monster.h"
 
+// Constructor!
 Monster::Monster(int hP, int aP)
 {
     health = hP;
@@ -13,22 +14,29 @@ Monster::Monster(int hP, int aP)
     }
 }
 
+// monster AttackPower mutator!
 void Monster::decreaseAttackPower()
 {
     attackPower = attackPower - 5;
 }
-void Monster::setHP(int h) {
+
+// monster Health mutator and accessor!
+void Monster::setHP(int h) 
+{
     health = h;
 }
-int Monster::getHP() const {
+int Monster::getHP() const 
+{
     return health;
 }
 
-//AttackPower mutator and accessor
-void Monster::setAttackPower(int ap) {
+// monster AttackPower mutator and accessor!
+void Monster::setAttackPower(int ap) 
+{
     attackPower = ap;
 }
 
-int Monster::getAttackPower() const {
+int Monster::getAttackPower() const 
+{
     return attackPower;
 }
