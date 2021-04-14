9836a8112ec5e48a32219af0f8f90e052626f030
LucasJAHH <80496226+LucasJAHH@users.noreply.github.com>
2021-04-14 00:03:52 -0400

    Update game.h

--- CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/game.h
+++ CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/game.h
@@ -4,21 +4,22 @@
 #include "room.h"
 using namespace std;
 
-class game{
-  private:
+class game 
+{
+  private: 
 	//private variables
 		Room* previousRoom;
 		Room* currentRoom;
-		Monster* monsters[3];
-		Room* rPtr;
-		//Hero heroName;
-
+		Room *rooms[8];
+		Monster* mPtr;
+		Hero* heroName = new Hero(75, 25, 10, 3);
 		char input;
 	//private functions
 		void createMap();
   public:
 	//public functions
 		void run();
+		void swap(Room*, Room*);
 	//constructors
 		game();
 };
