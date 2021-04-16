6e903b28c3056526fcfbd74c4fc46841fc8d3021
LucasJAHH <80496226+LucasJAHH@users.noreply.github.com>
2021-04-16 17:02:39 -0400

    Update room.cpp
    added comments

--- CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/room.cpp
+++ CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/room.cpp
@@ -1,148 +1,149 @@
 #include "room.h"
 
-Room::Room(string room_name, Room* n, Room* s, Room* e, Room* w, bool room)
-{
-	name = room_name;
-	north = n;
-	south = s;
-	east = e;
-	west = w;
-
-	if (room == true)
+// Start and exit room type constructor
+	Room::Room(string room_name, Room* n, Room* s, Room* e, Room* w, bool room)
 	{
-		exit = true;
+		name = room_name;
+		north = n;
+		south = s;
+		east = e;
+		west = w;
+
+		if (room == true)
+		{
+			exit = true;
+		}
 	}
 
-}
-
-Room::Room(string room_name, Room* n, Room* s, Room* e, Room* w, Monster* obj)
-{
-	name = room_name;
-	north = n;
-	south = s;
-	east = e;
-	west = w;
-
-	roomMonster = obj;
-}
-
-Room::Room(string room_name, Room* n, Room* s, Room* e, Room* w, Item* obj)
-{
-	name = room_name;
-	north = n;
-	south = s;
-	east = e;
-	west = w;
-
-	roomItem = obj;
-}
-
-Room::Room(string room_name, Room* n, Room* s, Room* e, Room* w, Monster* obj1, Item* obj2)
-{
-	name = room_name;
-	north = n;
-	south = s;
-	east = e;
-	west = w;
-	roomMonster = obj1;
-	roomItem = obj2;
-}
-
-
-//make sure these work and check if we need to set them to nullptr 
-
-void Room::setName(string room_name)
-{
-	name = room_name;
-}
-
-string Room::getName()const
-{
-	return name;
-}
-
-
-void Room::setWestRoom(Room* W)
-{
-	W->east = this->west;
-	west = W;
-	//cout << "have now set west.";
-}
-
-void Room::setEastRoom(Room* E)
-{
-	E->west = this->east;
-	east = E;
-	//cout << "room is set to east.";
-}
-
-void Room::setNorthRoom(Room* N)
-{
-	N->south = this->north;
-	north = N;
-	//cout << "have now set north.";
-}
-
-void Room::setSouthRoom(Room* S)
-{
-	S->north = this->south;
-	south = S;
-	//cout << "have now set south.";
-}
-
-Monster* Room::getRoomMonster() const
-{
-	return roomMonster;
-}
-
-Item* Room::getRoomItem() const
-{
-	return roomItem;
-}
-
-void Room::setRoomMonster(Monster *m)
-{
-	roomMonster = m;
-}
-
-void Room::setRoomItem(Item *t)
-{
-	roomItem = t;
-}
-
-Room* Room::getEastRoom()const //need to change to &'s ???
-{
-	//cout << name << " is in the east room." << endl;
-	return east;
-}
-
-Room* Room::getWestRoom()const
-{
-	//cout << name << " is in the west room." << endl;
-	return west;
-}
-
-Room* Room::getSouthRoom()const
-{
-	//cout << name << " is in the east room." << endl;
-	return south;
-}
-
-Room* Room::getNorthRoom()const
-{
-	//cout << name << " is in the north room." << endl;
-	return north;
-}
-
-bool Room::isExit()
-{
-	if (exit == true)
+// Monster type constructor
+	Room::Room(string room_name, Room* n, Room* s, Room* e, Room* w, Monster* obj)
 	{
-		//cout << endl << "exit" << endl;
-		return true;
+		name = room_name;
+		north = n;
+		south = s;
+		east = e;
+		west = w;
+
+		roomMonster = obj;
 	}
-	else {
-		//cout << endl << "not exit" << endl;
-		return false;
+
+//item room type constructor
+	Room::Room(string room_name, Room* n, Room* s, Room* e, Room* w, Item* obj)
+	{
+		name = room_name;
+		north = n;
+		south = s;
+		east = e;
+		west = w;
+
+		roomItem = obj;
 	}
-}
+
+//Monster and Item room type constructor
+	Room::Room(string room_name, Room* n, Room* s, Room* e, Room* w, Monster* obj1, Item* obj2)
+	{
+		name = room_name;
+		north = n;
+		south = s;
+		east = e;
+		west = w;
+		roomMonster = obj1;
+		roomItem = obj2;
+	}
+
+//name accessor and mutator
+	void Room::setName(string room_name)
+	{
+		name = room_name;
+	}
+
+	string Room::getName()const
+	{
+		return name;
+	}
+
+
+//movement option mutators
+	void Room::setWestRoom(Room* W)
+	{
+		west = W;
+		W->east = this;
+	}
+
+	void Room::setEastRoom(Room* E)
+	{
+		east = E;
+		E->west = this;
+	}
+
+	void Room::setNorthRoom(Room* N)
+	{
+		north = N;
+		N->south = this;
+	}
+
+	void Room::setSouthRoom(Room* S)
+	{
+		south = S;
+		S->north = this;
+	}
+
+//movement option accessors
+	Room* Room::getEastRoom()const
+	{
+		//cout << name << " is in the east room." << endl;
+		return east;
+	}
+
+	Room* Room::getWestRoom()const
+	{
+		//cout << name << " is in the west room." << endl;
+		return west;
+	}
+
+	Room* Room::getSouthRoom()const
+	{
+		//cout << name << " is in the east room." << endl;
+		return south;
+	}
+
+	Room* Room::getNorthRoom()const
+	{
+		//cout << name << " is in the north room." << endl;
+		return north;
+	}
+
+//room entity accessors and mutators
+	Monster* Room::getRoomMonster() const
+	{
+		return roomMonster;
+	}
+
+	Item* Room::getRoomItem() const
+	{
+		return roomItem;
+	}
+
+	void Room::setRoomMonster(Monster *m)
+	{
+		roomMonster = m;
+	}
+
+	void Room::setRoomItem(Item *t)
+	{
+		roomItem = t;
+	}
+
+//room is exit check
+	bool Room::isExit()
+	{
+		if (exit == true)
+		{
+			//cout << endl << "exit" << endl;
+			return true;
+		}
+		else {
+			//cout << endl << "not exit" << endl;
+			return false;
+		}
+	}
