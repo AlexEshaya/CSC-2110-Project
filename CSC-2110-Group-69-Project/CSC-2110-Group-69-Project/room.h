7db590f119487cea5a599c770ef39d9b52dea48e
LucasJAHH <80496226+LucasJAHH@users.noreply.github.com>
2021-04-16 17:03:36 -0400

    Update room.h

--- CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/room.h
+++ CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/room.h
@@ -3,40 +3,44 @@
 #include <iostream>
 using namespace std;
 
-class Room
-{
+class Room {
   private:
-  string name;
-  
-  Room* east;
-  Room* west;
-  Room* north;
-  Room* south;
-  
-  Monster* roomMonster;
-  Item* roomItem;
-  bool exit;
-  
+	//private variables
+		string name;
+		bool exit;
+		//movement option declaration
+		Room* east;
+		Room* west;
+		Room* north;
+		Room* south;
+		//room entity declaration
+		Monster* roomMonster;
+		Item* roomItem;
   public:
-  void setName(string);
-  string getName() const;
-  void setEastRoom(Room*);
-  void setWestRoom(Room*);
-  void setNorthRoom(Room*);
-  void setSouthRoom(Room*);
-  Room* getEastRoom() const;
-  Room* getWestRoom() const;
-  Room* getNorthRoom() const;
-  Room* getSouthRoom() const;
-  void setRoomMonster(Monster*);
-  Monster* getRoomMonster() const;
-  void setRoomItem(Item*);
-  Item* getRoomItem() const;
-  bool isExit();
+	//public functions
+		//name accessor and mutator
+		void setName(string);
+		string getName() const;
+		//movement option accessors and mutators
+		void setEastRoom(Room*);
+		void setWestRoom(Room*);
+		void setNorthRoom(Room*);
+		void setSouthRoom(Room*);
+		Room* getEastRoom() const;
+		Room* getWestRoom() const;
+		Room* getNorthRoom() const;
+		Room* getSouthRoom() const;
+		//room entity accessors and mutators
+		void setRoomMonster(Monster*);
+		Monster* getRoomMonster() const;
+		void setRoomItem(Item*);
+		Item* getRoomItem() const;
+		//room is exit check
+		bool isExit();
   
-  // Construct with parameters!
-  Room(string, Room*,Room*,Room*,Room*,Monster*,Item*);
-  Room(string, Room*,Room*,Room*,Room*,Monster*);
-  Room(string, Room*,Room*,Room*,Room*,Item*);
-  Room(string, Room*,Room*,Room*,Room*,bool);
+  //Constructors
+		Room(string, Room*,Room*,Room*,Room*,Monster*,Item*);
+		Room(string, Room*,Room*,Room*,Room*,Monster*);
+		Room(string, Room*,Room*,Room*,Room*,Item*);
+		Room(string, Room*,Room*,Room*,Room*,bool);
 };
