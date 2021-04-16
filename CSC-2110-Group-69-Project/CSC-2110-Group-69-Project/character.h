947eb5774449ab70eb48e2c334837b0cd61bee32
LucasJAHH <80496226+LucasJAHH@users.noreply.github.com>
2021-04-16 16:38:48 -0400

    Update character.h
    add more comments

--- CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/character.h
+++ CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/character.h
@@ -4,16 +4,17 @@ using namespace std;
 class Character
 {
 	public:
-		//HP mutator and accessor
-		virtual void setHP(int) = 0;
-		virtual int getHP() const = 0;
+		//Public functions
+			//HP mutator and accessor
+			virtual void setHP(int) = 0;
+			virtual int getHP() const = 0;
 	
-		//AttackPower mutator and accessor
-		virtual void setAttackPower(int) = 0;
-		virtual int getAttackPower() const = 0;
+			//AttackPower mutator and accessor
+			virtual void setAttackPower(int) = 0;
+			virtual int getAttackPower() const = 0;
 
 	protected:
-		//Variables
-		int health;
-		int attackPower;
+		//Protected Variables
+			int health;
+			int attackPower;
 };
