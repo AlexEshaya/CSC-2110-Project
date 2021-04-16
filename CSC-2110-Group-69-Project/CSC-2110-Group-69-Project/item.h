2116decb5f464c91af90a0bad4d775eb815fd229
LucasJAHH <80496226+LucasJAHH@users.noreply.github.com>
2021-04-16 16:56:16 -0400

    Update item.h

--- CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/item.h
+++ CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/item.h
@@ -1,10 +1,6 @@
 #pragma once
-#include <cstdlib> // For several general-purpose functions
-#include <fstream> // For file handling
-#include <iomanip> // For formatted output
 #include <iostream> // For cin, cout, and system
 #include <string> // For string data type
-#include <ctype.h> // For alphanumeric check
 using namespace std; // So "std::cout" may be abbreviated to "cout"
 
 // Declare Item Class
