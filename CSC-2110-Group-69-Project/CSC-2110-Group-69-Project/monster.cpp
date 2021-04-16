2060065f5a40a5930ec6ba3b88f1ba7789b9a7dd
LucasJAHH <80496226+LucasJAHH@users.noreply.github.com>
2021-04-16 16:57:23 -0400

    Update monster.cpp

--- CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/monster.cpp
+++ CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/monster.cpp
@@ -1,42 +1,45 @@
 #include "monster.h"
 
 // Constructor!
-Monster::Monster(int hP, int aP)
-{
-    health = hP;
-
-    if (aP >= 1)
+    Monster::Monster(int hP, int aP)
     {
-        attackPower = aP;
-    }
-    else {
-        attackPower = 0;
-    }
-}
+        health = hP;
 
+        if (aP >= 1)
+        {
+            attackPower = aP;
+        }
+        else {
+            attackPower = 0;
+        }
+    }
+
+//Monster Functions
+///////////////////////////////
 // monster AttackPower mutator!
-void Monster::decreaseAttackPower()
-{
-    attackPower = attackPower - 5;
-}
+    void Monster::decreaseAttackPower()
+    {
+        attackPower = attackPower - 5;
+    }
 
 // monster Health mutator and accessor!
-void Monster::setHP(int h) 
-{
-    health = h;
-}
-int Monster::getHP() const 
-{
-    return health;
-}
+    void Monster::setHP(int h) 
+    {
+        health = h;
+    }
+
+    int Monster::getHP() const 
+    {
+        return health;
+    }
 
 // monster AttackPower mutator and accessor!
-void Monster::setAttackPower(int ap) 
-{
-    attackPower = ap;
-}
+    void Monster::setAttackPower(int ap) 
+    {
+        attackPower = ap;
+    }
 
-int Monster::getAttackPower() const 
-{
-    return attackPower;
-}
+    int Monster::getAttackPower() const 
+    {
+        return attackPower;
+    }
