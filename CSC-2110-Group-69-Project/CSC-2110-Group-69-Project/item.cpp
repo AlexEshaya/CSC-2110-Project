109d7442c2bea698fb71469565b1744945335abf
LucasJAHH <80496226+LucasJAHH@users.noreply.github.com>
2021-04-16 16:55:01 -0400

    Update item.cpp

--- CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/item.cpp
+++ CSC-2110-Group-69-Project/CSC-2110-Group-69-Project/item.cpp
@@ -1,54 +1,54 @@
 #include "item.h"
 using namespace std; // So "std::cout" may be abbreviated to "cout"
 
-
-Item::Item(int TypeInput, int ValueInput)
-{
-  type = TypeInput; // Initialize type
-  value = ValueInput; // Initialize value
-}
+//Constructor
+  Item::Item(int TypeInput, int ValueInput)
+  {
+    type = TypeInput; // Initialize type
+    value = ValueInput; // Initialize value
+  }
 
 // Check if item is HP item
-bool Item::isHP() const
-{
-  if (type == 0)
+  bool Item::isHP() const
   {
-    return true;
+    if (type == 0)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
   }
-  else
-  {
-    return false;
-  }
-}
 
 // Check if item is defense item
-bool Item::isDefense() const
-{
-  if (type == 1)
+  bool Item::isDefense() const
   {
-    return true;
+    if (type == 1)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
   }
-  else
-  {
-    return false;
-  }
-}
 
 // Check if item is attack item
-bool Item::isAttack() const
-{
-  if (type == 2)
+  bool Item::isAttack() const
   {
-    return true;
+    if (type == 2)
+    {
+      return true;
+    }
+    else
+    {
+      return false;
+    }
   }
-  else
-  {
-    return false;
-  }
-}
 
 // Use accessor function to get value of item
-int Item::getValue() const
-{
-  return value;
-}
+  int Item::getValue() const
+  {
+    return value;
+  }
